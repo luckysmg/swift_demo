e914e13f0b5ce7b806ea8813ab2403601749fa0b
luckysmg <2539699336@qq.com>
2020-11-30 22:48:32 +0800

    更新

--- swift_demo/Controllers/PhotoCollectionViewController.swift
+++ swift_demo/Controllers/PhotoCollectionViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import AVKit
 import Photos
 
 class PhotoCell: UICollectionViewCell {
---
+++ swift_demo/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  swift_demo
+//
+//  Created by luckysmg on 2020/11/30.
+//
+
+import UIKit
+import AVKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
